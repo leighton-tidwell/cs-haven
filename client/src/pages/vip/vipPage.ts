d96f9056e287200b862f243f0628a6a31269e29e
Leighton Tidwell <leighton.tidwell57@gmail.com>
2022-12-05 15:57:16 -0600

    updating vip auth'

--- client/src/pages/vip/vipPage.ts
+++ client/src/pages/vip/vipPage.ts
@@ -4,10 +4,19 @@ import axios from "axios";
 let authData = null;
 
 const checkAuth = async () => {
-  const { data } = await axios.get("https://api.cs-haven.com/api/auth/verify");
-  console.log(data);
-  authData = reactive(data);
-  return data;
+  try {
+    const { data } = await axios.get(
+      "https://api.cs-haven.com/api/auth/verify",
+      {
+        withCredentials: true,
+      }
+    );
+    console.log(data);
+    authData = reactive(data);
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 export const vipPage = html`<button type="button" @click="${checkAuth}">
