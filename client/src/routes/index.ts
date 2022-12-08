a62c5d972fe77179b3dbcc9d229a840abef5a868
Leighton Tidwell <leighton.tidwell57@gmail.com>
2022-12-08 14:35:38 -0600

    testing

--- client/src/routes/index.ts
+++ client/src/routes/index.ts
@@ -1,4 +1,4 @@
-export { default as Vip } from "./vip/Vip";
-export { default as NotFound } from "./error404/Error404";
-export { default as Home } from "./home/Home";
-export { default as Root } from "./root";
+export { default as Vip } from "./vip/Vip.js";
+export { default as NotFound } from "./error404/Error404.js";
+export { default as Home } from "./home/Home.js";
+export { default as Root } from "./root.js";
